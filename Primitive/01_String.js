e957bc9fd93de7fce936a9a3535ff7a1d4c45473
Harsh Jain <113963702+harshjain0@users.noreply.github.com>
2024-11-16 21:09:05 +0530

    Add files via upload

---
+++ Primitive/01_String.js
@@ -0,0 +1,38 @@
+const name = "harsh"
+const repCount = 11
+// console.log(name + repCount+" Value");
+
+
+// console.log(`Hello my name is  ${name} and my repoCount is ${repCount}`);
+
+
+const gameName = new String("hello-hj-com");
+// console.log(gameName.split('-'));
+
+// console.log(gameName.bold());
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('l'));
+
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-1,3);
+// console.log(anotherString);
+
+// const str1 = "     harsh    ";
+// console.log(str1);
+// console.log(str1.trim());
+
+// const url = "http://harsh.com%34jain"
+// console.log(url.replace('%34','-'));
+// console.log(url.includes('harsh'));
